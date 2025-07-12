3757f05d7107875eecbc5950f978c503c8b4734b
bharath kumar <bharathpolampallisap@gmail.com>
2025-07-12 06:00:27 +0000

    commit

--- snapshots/sampl_snapshot.sql
+++ snapshots/sampl_snapshot.sql
@@ -1,9 +1,10 @@
-{% sanpshot sampl_snapshot %}
+{% snapshot sampl_snapshot %}
 
 {{config(strategy='timestamp',
-unique_key='emp_no',
-updated_at='updated_at',
-invalidate_hard_deletes=True)}}
+          unique_key='emp_no',
+          updated_at='updated_at',
+          invalidate_hard_deletes=True
+)}}
 
 select * from {{source('sample','EMPLOYEES')}}
 
